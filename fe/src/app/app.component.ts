a58c41fd637f47ed4b9358f3737004d04557b198
Farooq Mahmud <49829654+farooq-teqniqly@users.noreply.github.com>
2025-01-17 23:22:14 -0800

    Todo application

---
+++ fe/src/app/app.component.ts
@@ -0,0 +1,86 @@
+// noinspection AngularUnusedComponentImport
+
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TodoList } from "./todoList";
+import { TodoItem } from "./todoItem";
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
+import { MatBadge } from "@angular/material/badge";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from "@angular/material/table";
+import { MatCheckbox } from "@angular/material/checkbox";
+import { FormsModule } from "@angular/forms";
+import { MatFormField } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { MatButton } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSlideToggle } from "@angular/material/slide-toggle";
+
+@Component({
+  selector: "app-root",
+  imports: [
+    CommonModule,
+    MatToolbar,
+    MatIcon,
+    MatBadge,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    MatCheckbox,
+    FormsModule,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggle,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+})
+export class AppComponent {
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+  ]);
+
+  showComplete = false;
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter((item) => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(
+      (item) => this.showComplete || !item.complete
+    );
+  }
+
+  addItem(task: string) {
+    this.list.addItem(task);
+  }
+}
