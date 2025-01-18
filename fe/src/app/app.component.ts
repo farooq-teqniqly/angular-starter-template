e5bffb7a15ada4721c6d058b114cb272c211dcf4
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2025-01-17 22:27:51 -0800

    chore: Add Angular Material and refactor UI using Material components.

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from "@angular/core";
 import { TodoList } from "./todoList";
 import { TodoItem } from "./todoItem";
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
+import { MatBadge } from "@angular/material/badge";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from "@angular/material/table";
 
 @Component({
   selector: "app-root",
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatBadge,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+  ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
@@ -21,5 +50,9 @@ export class AppComponent {
 
   get itemCount(): number {
     return this.list.items.filter((item) => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
   }
 }
