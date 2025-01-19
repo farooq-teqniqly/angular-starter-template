b58c7526bbc20681a9029338a3e6738a67917a8e
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2025-01-19 14:51:45 -0800

    chore: Added Sports Store application.

--- sportsstore/src/app/app.component.ts
+++ sportsstore/src/app/app.component.ts
@@ -2,10 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  imports: [],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = "sportsstore";
+  title = "Sports Store";
 }
